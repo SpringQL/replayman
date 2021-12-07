def76fc266e93c963157c9734c5964255067e10b
Sho Nakatani <lay.sakura@gmail.com>
2021-12-07 16:20:55 +0900

    feat: cargo run then read file from local FS

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use chrono::DateTime;
+use springql_foreign_service::source::source_input::{
+    timed_stream::{file_type::FileType, TimedStream},
+    ForeignSourceInput,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let timed_stream = TimedStream::new(
+        FileType::Tsv,
+        "/Users/sho.nakatani/.ghq/src/github.com/SpringQL/dataset/pseudo-in-vehicle/AirConditioner-30sec.tsv",
+        "Time".to_string(),
+        DateTime::parse_from_rfc3339("2020-10-21T10:37:56.000+09:00").unwrap(),
+    )
+    .unwrap();
+
+    let input = ForeignSourceInput::new_timed_stream(timed_stream);
+
+    for j in input {
+        println!("{}", j.unwrap());
+    }
 }
